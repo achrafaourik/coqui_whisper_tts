1df8f0aa215641cbb1eaf6a9dadea380ec4cee29
Achraf <aaourik@pca.ma>
2023-08-02 16:19:51 +0100

    fix bash command

--- bash_script.sh
+++ bash_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && apt upgrade -y && apt install -y jupyter tmux htop vim zip unzip git-lfs && apt remove -y python3-blinker && pip install TTS && wget https://files.catbox.moe/59mnzq.zip -O drive.zip && unzip drive.zip && python download_drive_file.py 1YnnWIKdNGUQkbeziKcSz1eSRWfnzwiZ5 ./bark_voices/speaker/airi_0.wav && git lfs clone https://huggingface.co/suno/bark/tree/main
+apt update && apt upgrade -y && apt install -y jupyter tmux htop vim zip unzip git-lfs && apt remove -y python3-blinker && pip install TTS && wget https://files.catbox.moe/59mnzq.zip -O drive.zip && unzip drive.zip && python download_file_drive.py 1YnnWIKdNGUQkbeziKcSz1eSRWfnzwiZ5 ./bark_voices/speaker/airi_0.wav && git lfs clone https://huggingface.co/suno/bark/tree/main
 
 
 exec "$@"
