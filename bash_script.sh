a603c6fabb31e30a2bbe9b7823eaa046300d4746
Achraf <aaourik@pca.ma>
2023-08-05 22:46:47 +0100

    update bash script installs

--- bash_script.sh
+++ bash_script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-apt update && apt upgrade -y && apt install -y jupyter tmux htop vim zip unzip git-lfs && apt remove -y python3-blinker && pip install TTS && wget https://files.catbox.moe/59mnzq.zip -O drive.zip && unzip drive.zip && python download_file_drive.py 1YnnWIKdNGUQkbeziKcSz1eSRWfnzwiZ5 ./bark_voices/speaker/airi_0.wav && git lfs clone https://huggingface.co/suno/bark
+apt update && apt upgrade -y && apt install -y jupyter tmux htop vim zip unzip git-lfs && apt remove -y python3-blinker
+pip install TTS google-api-python-client
+wget https://files.catbox.moe/59mnzq.zip -O drive.zip && unzip drive.zip && python download_file_drive.py 1YnnWIKdNGUQkbeziKcSz1eSRWfnzwiZ5 ./bark_voices/speaker/airi_0.wav
+git lfs clone https://huggingface.co/suno/bark
 
 
 exec "$@"
